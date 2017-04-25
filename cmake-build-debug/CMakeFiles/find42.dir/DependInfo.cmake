142c3be22b90aee1bc363444832785fe2ad5d12a
Phil Oetinger <p.oetinger@hotmail.com>
2017-04-25 14:52:44 +0200

    Initial Upload

---
+++ cmake-build-debug/CMakeFiles/find42.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/PhilO/Documents/GitHub/find42/fringe.c" "C:/Users/PhilO/Documents/GitHub/find42/cmake-build-debug/CMakeFiles/find42.dir/fringe.c.obj"
+  "C:/Users/PhilO/Documents/GitHub/find42/search.c" "C:/Users/PhilO/Documents/GitHub/find42/cmake-build-debug/CMakeFiles/find42.dir/search.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/find42.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/find42.dir/fringe.c.obj"
+  "CMakeFiles/find42.dir/search.c.obj"
+  "find42.pdb"
+  "find42.exe"
+  "find42.exe.manifest"
+  "libfind42.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/find42.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
