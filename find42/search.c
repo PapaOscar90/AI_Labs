956ad4567cdc7155f3e3501ff41cf2892a5c98ce
Hydro7 <said.faroghi@gmail.com>
2017-05-04 13:41:26 +0200

    How to do 3.3 without pointers :I

--- find42/search.c
+++ find42/search.c
@@ -6,13 +6,26 @@
 #include "fringe.h"
 
 #define RANGE 1000000
+#define MAXV 500000 // Max Visited List size
 
-Fringe insertValidSucc(Fringe fringe, int value) {
+
+int find(int *array, int size, int element){
+	for (int i = 0; i < size; i++){
+		if (array[i] == element){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+Fringe insertValidSucc(Fringe fringe, int value, int **visitedList, int visited) {
   State s;
-  if ((value < 0) || (value > RANGE)) {
-    /* ignore states that are out of bounds */
+  if ((value < 0) || (value > RANGE) || find(*visitedList, visited, value)) {
+    /* ignore states that are out of bounds, or
+     * are already visited */
     return fringe;
   }
+  (*visitedList)[visited] = value;
   s.value = value;
   return insertFringe(fringe, s);
 }
@@ -22,28 +35,30 @@ void search(int mode, int start, int goal) {
   State state;
   int goalReached = 0;
   int visited = 0;
+  int *visitedList = malloc(MAXV*sizeof(int));
   int value;
 
   fringe = makeFringe(mode);
   state.value = start;
-  fringe = insertFringe(fringe, state);
+  fringe = insertFringe(fringe, state, &visitedList, visited);
   while (!isEmptyFringe(fringe)) {
     /* get a state from the fringe */
     fringe = removeFringe(fringe, &state);
-    visited++;
     /* is state the goal? */
     value = state.value;
+    visitedList[visited] = value;
+    visited++;
     if (value == goal) {
       goalReached = 1;
       break;
     }
-    /* insert neighbouring states */ // TODO: Change to DFS style, go deep before shallow for better memory use
-    fringe = insertValidSucc(fringe, value+1); /* rule n->n + 1      */
-    fringe = insertValidSucc(fringe, 2*value); /* rule n->2*n        */
-    fringe = insertValidSucc(fringe, 3*value); /* rule n->3*n        */
-    fringe = insertValidSucc(fringe, value-1); /* rule n->n - 1      */
-    fringe = insertValidSucc(fringe, value/2); /* rule n->floor(n/2) */
-    fringe = insertValidSucc(fringe, value/3); /* rule n->floor(n/3) */
+    /* insert neighbouring states */
+    fringe = insertValidSucc(fringe, value+1, &visitedList, visited); /* rule n->n + 1      */
+    fringe = insertValidSucc(fringe, 2*value, &visitedList, visited); /* rule n->2*n        */
+    fringe = insertValidSucc(fringe, 3*value, &visitedList, visited); /* rule n->3*n        */
+    fringe = insertValidSucc(fringe, value-1, &visitedList, visited); /* rule n->n - 1      */
+    fringe = insertValidSucc(fringe, value/2, &visitedList, visited); /* rule n->floor(n/2) */
+    fringe = insertValidSucc(fringe, value/3, &visitedList, visited); /* rule n->floor(n/3) */
   }
   if (goalReached == 0) {
     printf("goal not reachable ");
@@ -52,7 +67,8 @@ void search(int mode, int start, int goal) {
   }
   printf("(%d nodes visited)\n", visited);
   showStats(fringe);
-  deallocFringe(fringe);  
+  deallocFringe(fringe);
+  free(visitedList);
 }
 
 int main(int argc, char *argv[]) {
