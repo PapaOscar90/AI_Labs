cb62c3d34950269078b984875a380006fe74b1cf
Phil Oetinger <p.oetinger@student.rug.nl>
2017-05-05 12:49:22 +0200

    Tried implementing Astar, didn't go well. But it is in the right direction.

--- knights/idknight.c
+++ knights/idknight.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #define N 500   /* N times N chessboard */
 
@@ -8,6 +9,71 @@ int actions[8][2] = {  /* knight moves */
 };
 int costShortestPath[N][N];
 unsigned long statesVisited = 0;
+
+void doubleQSize(struct Queue q);
+
+typedef struct position{
+    int x,y;
+    int visited;
+    struct position *camefrom;
+    int costRemaining;
+}position;
+
+typedef struct Queue{
+    position *array;
+    int back;
+    int front;
+    int size;
+}Queue;
+
+Queue newQueue(int s){
+    Queue q;
+    q.array=malloc(s*sizeof(position));
+    assert(q.array != NULL);
+    q.back = 0;
+    q.front = 0;
+    q.size = s;
+    return
+}
+
+void enqueue(Queue q, position p){
+    q.back +=1;
+    if(q.back >= q.size){
+        doubleQSize(q);
+    }
+    q.array[q.back] = p;
+    q.size++;
+}
+
+position dequeue(Queue q){
+    q.front += 1;
+    q.size--;
+    return q.array[q.front-1];
+}
+
+position dequeueLeast(Queue q){
+    int min=99999999;
+    int minIndex;
+    for(int i = 0; i<q.size; i++){
+        if(q.array[i]<min){
+            minIndex = i;
+            min = q.array[i];
+        }
+    }
+    position returnPosition = q.array[minIndex];
+
+    for (int i = minIndex+1; i<q.size; i++){
+        i>0 ? (q.array[i - 1] = q.array[i]) : (q.array[q.front] += 1);
+    }
+    q.size--;
+    return returnPosition;
+}
+
+void doubleQSize(Queue q) {
+    q.array=realloc((q.size*2), sizeof(position));
+    assert(q.array != NULL);
+    q.size *=2;
+}
 
 int isValidLocation(int x, int y) {
   return (0<=x && x < N && 0<= y && y < N);
@@ -21,6 +87,7 @@ void initialize() {
     }
   }
 }
+
 
 int knightDLS(int cost, int limit, int row, int column, int rowGoal, int columnGoal) {
   int act;
@@ -53,6 +120,46 @@ int knightIDS(int row, int column, int rowGoal, int columnGoal) {
   }
   printf("\n");
   return limit;
+}
+
+int aStar(int row, int column, int rowGoal, int columnGoal, int goalDistance){
+    Queue checkedList = newQueue(0);
+    Queue toCheckList = newQueue(0);
+    position start;
+    start.x = row;
+    start.y = column;
+    start.camefrom = NULL;
+    start.visited=0;
+    start.costRemaining = (rowGoal-row) + (columnGoal-column);
+
+    enqueue(toCheckList, start);
+    position currentPosition;
+    position child;
+    while (Queue.size >= 0) {
+        if (Queue.size == 0){
+            return 0;
+        }
+        currentPosition = dequeueLeast(toCheckList);
+        if (currentPosition.x == rowGoal && currentPosition.y == columnGoal){
+            printTheSolution(currentPosition);
+        }
+        currentPosition.visited=1;
+        for(int i=0; i<8; i++){
+            child.x = currentPosition.x + actions[i][0];
+            child.y = currentPosition.y + actions[i][1];
+            if (isValidLocation(child.x, child.y) && child.visited==0){
+                enqueue(toCheckList, child);
+            }
+        }
+    }
+
+}
+
+void printTheSolution(position p){
+    while (p->camefrom != NULL){
+        printf("(%d,%d)-> ", p.x, p.y);
+    }
+    printf("Start");
 }
 
 int main(int argc, char *argv[]) {
