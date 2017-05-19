ec793a6d901be327cdb4181d0eb07d6d6a6397c8
Phil Oetinger <p.oetinger@student.rug.nl>
2017-05-19 15:16:41 +0200

    Final

--- Assignment 2/Nim/nim(1).c
+++ Assignment 2/Nim/nim(1).c
@@ -5,38 +5,48 @@
 #define MIN -1
 #define INFINITY 9999999
 
-int recursiveBestMove=3;
+int recursiveBestMove;
+int beginingState;
 int lookUpTable[100];
 
 int negaMaxDecision(int state, int color) {
     int result;
 
-    if(lookUpTable[state] != 0){
-        return (lookUpTable[state]);
+    if (state == 1) {
+        lookUpTable[state]=-1*color;
+        return (-1*color);
     }
 
-    if (state == 1) {
-        lookUpTable[state]=-1;
-        return (-1 * color);
-    }
 
     int v = -INFINITY;
-    for (int i = 1; i <4; i++) {
-        if (state - i >0) {
-            result = -1 * negaMaxDecision(state - i, -1 * color);
+    for (int i = 1; i < 4; i++) {
+        if (state - i >=0) {
+            if (state - i == 0){
+                result = -1*color;
+            }else {
+                if (lookUpTable[state-i] !=0){
+                    result = color * lookUpTable[state-i];
+                }else {
+                    result = -1 * negaMaxDecision(state - i, -1 * color);
+                }
+            }
             if (v <= result) {
+                if(beginingState == state) {
+                    recursiveBestMove = i;// Thought this would just trace back with the best moves until the last one, then return i
+                }
                 v = result;
-                recursiveBestMove = i;// Thought this would just trace back with the best moves until the last one, then return i
             }
         }
     }
-    lookUpTable[state]=v;
+    lookUpTable[state]=v*color;
     return v;
 }
 
 void playNim(int state) {
     int turn = 1;
     while (state > 1) {
+        beginingState=state;
+        recursiveBestMove=1;
         int action = negaMaxDecision(state, turn);
         printf("%d: %s takes %d\n", state,
                (turn == MAX ? "Max" : "Min"), recursiveBestMove);
